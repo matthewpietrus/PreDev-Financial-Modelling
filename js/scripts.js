eb3fc1e329d3a68d04bf8444b1323c46d9dcd8ce
matthewpietrus <43551412+matthewpietrus@users.noreply.github.com>
2021-02-26 10:21:33 -0500

    Corrected sliders to make them interact
    Slider two is now dependent on the max of slider one.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,40 @@
+// input type = number control for input to make it a number. Maybe get rid of first bar and replace with input. 
+
 var slidertdc = document.getElementById("totaldevcost");
-var outputtdc = document.getElementById("tdc");
-outputtdc.innerHTML = slidertdc.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slidertdc.oninput = function() {
-  outputtdc.innerHTML = this.value;
-}
 var sliderequity = document.getElementById("equity");
+
+var outputtdc = document.getElementById("tdc");
 var outputequity = document.getElementById("invest");
-outputequity.innerHTML = sliderequity.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-sliderequity.oninput = function() {
-  outputequity.innerHTML = this.value;
-}
-var sliderdebt = document.getElementById("debt");
 var outputdebt = document.getElementById("owed");
-outputdebt.innerHTML = sliderdebt.value; // Display the default slider value
+
+outputtdc.innerHTML = slidertdc.value; // Display the default slider value
+outputequity.innerHTML = sliderequity.value; // Display the default slider value
+outputdebt.innerHTML=Math.round((slidertdc.value-sliderequity.value)*10)/10; // Display the default slider value
+
+// // Update the current slider value (each time you drag the slider handle)
+// slidertdc.oninput = function() {
+//   outputtdc.innerHTML = this.value;
+//   sliderequity.max=this.value
+//   sliderdebt.max=this.value
+// }
+//
+// // Update the current slider value (each time you drag the slider handle)
+// sliderequity.oninput = function() {
+//   outputequity.innerHTML = this.value;
+// }
+//
+// // Update the current slider value (each time you drag the slider handle)
+// sliderdebt.oninput = function() {
+//   outputdebt.innerHTML = this.value;
+// }
 
 // Update the current slider value (each time you drag the slider handle)
-sliderdebt.oninput = function() {
-  outputdebt.innerHTML = this.value;
+var updatevalues=function() {
+  outputtdc.innerHTML = slidertdc.value;
+  outputequity.innerHTML = sliderequity.value;
+  sliderequity.max=slidertdc.value;
+  outputdebt.innerHTML=Math.round((slidertdc.value-sliderequity.value)*10)/10
 }
+
+slidertdc.oninput = updatevalues
+sliderequity.oninput = updatevalues
